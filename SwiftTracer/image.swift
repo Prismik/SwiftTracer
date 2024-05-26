746d0c4bfaed61298ad75da7cc9fde807e5af17d
Francis <francis.beauchampsc@gmail.com>
2024-05-26 19:44:13 -0400

    Add simple image wrapper, code to write to file, normal integrator

---
+++ SwiftTracer/image.swift
@@ -0,0 +1,8 @@
+//
+//  image.swift
+//  SwiftTracer
+//
+//  Created by Francis Beauchamp on 2024-05-26.
+//
+
+import Foundation
---
+++ SwiftTracer/integrators/normal.swift
@@ -0,0 +1,8 @@
+//
+//  normal.swift
+//  SwiftTracer
+//
+//  Created by Francis Beauchamp on 2024-05-26.
+//
+
+import Foundation
